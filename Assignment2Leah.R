14c55c1de514cef5c6ae40a30acc57c157ad5abb
LeahJS14 <leahjshapiro@gmail.com>
2020-10-01 21:29:08 -0400

    exploring data

---
+++ Assignment2Leah.R
@@ -0,0 +1,136 @@
+# Load Libraries
+library(tidyverse)
+library(sf)
+library(spdep)
+library(caret)
+library(ckanr)
+library(FNN)
+library(grid)
+library(gridExtra)
+library(ggcorrplot)
+library(jtools)  
+library(viridis)
+
+
+# Themes and Functions
+mapTheme <- function(base_size = 12) {
+  theme(
+    text = element_text( color = "black"),
+    plot.title = element_text(size = 14,colour = "black"),
+    plot.subtitle=element_text(face="italic"),
+    plot.caption=element_text(hjust=0),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(),axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill=NA, size=2)
+  )
+}
+
+plotTheme <- function(base_size = 12) {
+  theme(
+    text = element_text( color = "black"),
+    plot.title = element_text(size = 14,colour = "black"),
+    plot.subtitle = element_text(face="italic"),
+    plot.caption = element_text(hjust=0),
+    axis.ticks = element_blank(),
+    panel.background = element_blank(),
+    panel.grid.major = element_line("grey80", size = 0.1),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill=NA, size=2),
+    strip.background = element_rect(fill = "grey80", color = "white"),
+    strip.text = element_text(size=12),
+    axis.title = element_text(size=12),
+    axis.text = element_text(size=10),
+    plot.background = element_blank(),
+    legend.background = element_blank(),
+    legend.title = element_text(colour = "black", face = "italic"),
+    legend.text = element_text(colour = "black", face = "italic"),
+    strip.text.x = element_text(size = 14)
+  )
+}
+
+palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
+
+qBr <- function(df, variable, rnd) {
+  if (missing(rnd)) {
+    as.character(quantile(round(df[[variable]],0),
+                          c(.01,.2,.4,.6,.8), na.rm=T))
+  } else if (rnd == FALSE | rnd == F) {
+    as.character(formatC(quantile(df[[variable]]), digits = 3),
+                 c(.01,.2,.4,.6,.8), na.rm=T)
+  }
+}
+
+q5 <- function(variable) {as.factor(ntile(variable, 5))}
+
+nn_function <- function(measureFrom,measureTo,k) {
+  measureFrom_Matrix <- as.matrix(measureFrom)
+  measureTo_Matrix <- as.matrix(measureTo)
+  nn <-   
+    get.knnx(measureTo, measureFrom, k)$nn.dist
+  output <-
+    as.data.frame(nn) %>%
+    rownames_to_column(var = "thisPoint") %>%
+    gather(points, point_distance, V1:ncol(.)) %>%
+    arrange(as.numeric(thisPoint)) %>%
+    group_by(thisPoint) %>%
+    summarize(pointDistance = mean(point_distance)) %>%
+    arrange(as.numeric(thisPoint)) %>% 
+    dplyr::select(-thisPoint) %>%
+    pull()
+  
+  return(output)  
+}
+
+# Data Wrangling
+
+MiamiDF <- st_read("C:/Users/owner160829a/Documents/GitHub/MUSA-508-Assignment2/studentsData.geojson")
+
+MiamiDF <- st_transform(MiamiTrainingDF,'ESRI:102658') #%>% 
+#select(-saleQual,-GPAR,-Land.Use,-Owner1,-Owner2,-saleDate,-Year)
+
+MiamiTrainingDF <- MiamiDF[!(MiamiDF$SalePrice==0),]
+
+Neighborhoods <- st_read("https://opendata.arcgis.com/datasets/2f54a0cbd67046f2bd100fb735176e6c_0.geojson")%>%
+  st_transform('ESRI:102658')
+
+# Maps
+
+ggplot() +
+  geom_sf(data=Neighborhoods)+
+ geom_sf(data=MiamiTrainingDF)
+
+
+ggplot() +
+  geom_sf(data = Neighborhoods, fill = "grey40") +
+  geom_sf(data = MiamiTrainingDF, aes(colour = q5(SalePrice)), 
+          show.legend = "point", size = 1) +
+  scale_colour_manual(values=palette5,
+                      labels=qBr(MiamiTrainingDF,"SalePrice"),
+                      name="Quintile\nBreaks") +
+  labs(title="Sale Price, Miami") +
+  mapTheme()
+  
+
+# Correlation Matrix
+
+numericVars <- 
+  select_if(st_drop_geometry(MiamiTrainingDF), is.numeric) %>% na.omit() %>%
+  select(SalePrice,Bed, Bath, Stories, Units, YearBuilt, LivingSqFt)
+
+ggcorrplot(
+  round(cor(numericVars), 1), 
+  p.mat = cor_pmat(numericVars),
+  colors = c("#25CB10", "white", "#FA7800"),
+  type="lower",
+  insig = "blank") +  
+  labs(title = "Correlation across numeric variables") 
+
+# Regression
+reg <- lm(SalePrice ~ ., data = st_drop_geometry(MiamiTrainingDF) %>% 
+             dplyr::select(SalePrice, Bed, Bath, Stories, YearBuilt,LivingSqFt))
+summ(reg)
+summary(reg)
